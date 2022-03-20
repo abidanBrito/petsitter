37e6bbd83910f6cd7e41527a02a2495509c26429
Ivan Villanueva <ivandiscobolo@gmail.com>
2022-03-20 21:01:21 +0100

    H1-03/feat: Added SLAM 01 package

---
+++ petsitter/petsitter_slam_01/launch/petsitter_slam_01.launch.py
@@ -0,0 +1,49 @@
+#my_slam.launch.py
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import SetEnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    rviz_config_dir = os.path.join(get_package_share_directory('petsitter_slam_01'), 'rviz', 'my_slam.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        #SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM','1'),
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=[
+                '-configuration_directory', get_package_share_directory('petsitter_slam_01') + '/config',
+                '-configuration_basename','my_slam.lua'
+            ],
+        ),
+        
+        Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+            ),
+            
+        Node(
+            package='cartographer_ros',
+            executable='occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments = ['-resolution','0.05','-publish_period_sec','1.0']
+        ),
+    ]) 
