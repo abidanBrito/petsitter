6fa578676c1350233f8a36629e9892e7326b062d
ruben021000 <55706771+ruben021000@users.noreply.github.com>
2022-06-07 19:14:47 +0200

    Add files via upload
    added the service

--- petsitter_ros2/petsitter/petsitter_capture_image/petsitter_capture_image/photo_opencv.py
+++ petsitter_ros2/petsitter/petsitter_capture_image/petsitter_capture_image/photo_opencv.py
@@ -12,212 +12,18 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
 import pyrebase
+from datetime import datetime
 
 # Importar mensajes
 from geometry_msgs.msg import Twist
 from petsitter_custom_interface.srv import OpencvMsg
 
-'''
-class Ros2OpenCVImageConverter(Node):   
 
-    def __init__(self):
-
-        super().__init__('Ros2OpenCVImageConverter')
-        self.resultado = 10
-        self.analize = False
-
-        self.bridge_object = CvBridge()
-        self.image_sub = self.create_subscription(Image,'/camera/image_raw',self.camera_callback,QoSProfile(depth=1, reliability=ReliabilityPolicy.BEST_EFFORT))
-        
-
-
-    def camera_callback(self,data):
-
-        if self.analize == True:
-
-            try:
-                # Seleccionamos bgr8 porque es la codificacion de OpenCV por defecto
-                cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
-            except CvBridgeError as e:
-                print(e)
-
-            cv2.imshow("Imagen capturada por el robot", cv_image)
-                
-            cv2.waitKey(1)    
-            cv2.imwrite("/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_web/imgs/Imagen_cuenco.jpg", cv_image)
-        
-            #Opencv
-            #Cargamos las dos imágenes
-            img1 = cv2.imread("/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_web/imgs/Imagen_cuenco.jpg")
-            img2 = cv2.imread("/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_web/imgs/Imagen_cuenco.jpg")
-
-            #Comparamos cada imagen con la original mediate subtract: sustrae cada píxel y valor de color
-            diff_12 = cv2.subtract(img1,img2)
-
-            #Sumamos todos los elementos del array diferencia para obtener un número representativo de cómo de similares o diferentes son
-            print("--------------------------------------------------------")
-            print("DIFERENCIA: ")
-            resto = np.sum(diff_12)
-            print(resto)
-
-            if(resto < 5000000):
-                valor_diferencia = 0
-                print("0%")
-            elif(resto >= 5000000 and resto <= 7000000):
-                valor_diferencia = 33
-                print("33%")
-            elif(resto > 7000001 and resto <= 10075000):
-                valor_diferencia = 66
-                print("66%")
-            elif(resto > 10075001 and resto <= 10700000):
-                valor_diferencia = 100
-                print("100%")
-            else:
-                print("Hay demasiada cantidad de comida")
-                valor_diferencia = 100000
-
-        #--------------------------------------------------------------------------------------------------
-            hsv = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
-            dark_blue=np.uint8([[[255,0,0]]])
-            hsv_dark_blue = cv2.cvtColor(dark_blue,cv2.COLOR_BGR2HSV)
-
-            lower_brown = np.array([0,140,0])#rango de color marrón que se capta
-            upper_brown = np.array([255,255,255])
-
-            #cv2.imshow('hsv',hsv)
-
-            mask = cv2.inRange(hsv, lower_brown, upper_brown)
-            #cv2.imshow('mask',mask)
-
-            res = cv2.bitwise_and(img2, img2, mask= mask)
-            #cv2.imshow('res',res)
-
-            #cantidad de pixeles marrones
-            total = res.size
-            #print(total) #píxeles verticales y horizontales
-
-            num_black_px = np.sum(res == 0)
-            #print(num_black_px)#pixeles negros
-
-            px_marron = total - num_black_px
-            print("--------------------------------------------------------")
-            print("PIXELES MARRONES: ")
-            print(px_marron)#pixeles marrones
-
-            if(px_marron < 50000):
-                valor_color = 0
-                print("0%")
-            elif(px_marron >= 50000 and px_marron <= 90000):
-                valor_color = 33
-                print("33%")
-            elif(px_marron > 90001 and px_marron <= 110350):
-                valor_color = 66
-                print("66%")
-            elif(px_marron > 110351 and px_marron <= 130000):
-                valor_color = 100
-                print("100%")
-            else:
-                print("Hay demasiada cantidad de comida")
-                valor_color = 100000
-
-            print("--------------------------------------------------------")
-
-            if(valor_diferencia == valor_color):
-                valor_final = valor_color
-            elif(valor_diferencia > valor_color):
-                valor_final = valor_color
-            else:
-                valor_final = valor_diferencia
-
-            print("VALOR FIABLE:")
-            print(valor_final, "%")
-            print("--------------------------------------------------------")
-
-        #cv2.waitKey(0) #aprieta una tecla 
-        #cv2.destroyAllWindows()
-
-            self.resultado = valor_final
-            print(self.resultado)
-
-            def get_resultado(self):
-                print(" resultado del metodo")
-                print(self.resultado)
-
-            return self.resultado
-
-
-
-def main(args=None):
-
-    rclpy.init(args=args)    
-    img_converter_object = Ros2OpenCVImageConverter()    
-       
-    try:
-        print("Nueva imagen")
-        rclpy.spin_once(img_converter_object)#Se ejecuta una vez
-
-
-         # credenciales para Firestore con firebase_admin
-        cred = credentials.Certificate("/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_ros2/petsitter/petsitter_capture_image/petsitter_capture_image/firebase-sdk.json")
-        firebase_admin.initialize_app(cred)
-
-        # credenciales para Storage con pyrebase
-
-        config = {
-            'apiKey': "AIzaSyABOCK6NTYF1jsv1WT5SiDZdFgUuhXLH2o",
-
-            'authDomain': "petsitter-42488.firebaseapp.com",
-
-            'projectId': "petsitter-42488",
-
-            'storageBucket': "petsitter-42488.appspot.com",
-
-            'messagingSenderId': "590637458117",
-
-            'appId': "1:590637458117:web:e0681e78d84b708a3c46ac",
-
-            "databaseURL": ""
-        }
-
-        firebase = pyrebase.initialize_app(config)
-
-        # subir imagen a Storage
-
-        storage = firebase.storage()
-
-        my_image = "/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_web/imgs/Imagen_cuenco.jpg"
-        image_path = "/images/Imagen_cuenco.jpg"
-
-        # Upload image
-        storage.child(image_path).put(my_image)
-        img_url = storage.child(image_path).get_url(None)
-
-        # subir documento a Firestore
-
-        db = firestore.client()
-
-        doc_ref = db.collection(u'Imagenes').document(u'imagen_cuenco')
-        doc_ref.set({
-            u'url' : img_url,
-            u'cantidad': img_converter_object.get_resultado()
-        })
-
-        
-    except KeyboardInterrupt:
-        img_converter_object.destroy_node()
-        print("Fin del programa!")
-    
-    cv2.destroyAllWindows() 
-    
-
-if __name__ == '__main__':
-    main()
-
-'''
-
- # credenciales para Firestore con firebase_admin
+# credenciales para Firestore con firebase_admin
 cred = credentials.Certificate("/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_ros2/petsitter/petsitter_capture_image/petsitter_capture_image/firebase-sdk.json")
 firebase_admin.initialize_app(cred)
+
+
 
         # credenciales para Storage con pyrebase
 
@@ -239,38 +45,59 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-class Service(Node):
+class ServiceOpencv(Node):
 
-    analize = False
+    """
+  	Nodo que crea un servicio para tomar una foto y analizarla
+
+  	Attributes:
+    	srv (service): servicio del servidor
+    	resultado (float): valor final del escaneado (0/33/66/100)%
+        bridge: puente
+        image_sub: lanza el topic y la funcion camera_callback
+        analize (bool): indica cuando hay que analizar la imagen
+  
+  	Methods:
+    	camera_callback(): callback de la suscripción al topic '/camera/image_raw'
+        my_firts_service_callback(): callback del servicio. Comienza a analizar al recibir el request 'analize = True'
+
+  	"""
 
     def __init__(self):
         #constructor con el nombre del nodo
         super().__init__('Ros2OpenCVImageConverter') 
-        # declara el objeto servicio pasando como parametros
-        # tipo de mensaje
-        # nombre del servicio
-        # callback del servicio
+       
         self.resultado = 10
-        self.analize = False
-
+        #image para el topic real
         self.bridge_object = CvBridge()
         self.image_sub = self.create_subscription(Image,'/camera/image_raw',self.camera_callback,QoSProfile(depth=1, reliability=ReliabilityPolicy.BEST_EFFORT))
         self.srv = self.create_service(OpencvMsg, 'analize', self.my_first_service_callback)
 
-        #declara el objeto publisher pasando como parametros
-        # tipo de mensaje
-        # nombre del topic
-        # tamaño de la cola
+        self.analize = False
 
-        #self.publisher = self.create_publisher(Twist)
+    #print("Entra en el servicio")
 
     def camera_callback(self,data):
+        #print("Entra al callback")
+        """ 
+        Callback del listener de la suscripción al topic '/camera/image_raw' Si analize = True entonces toma una 
+        foto y comienza a analizarla con opencv. Por una parte mira la diferencia entre un cuenco vacio y el actual 
+        y por otra la cantidad de marron que hay, es decir la cantidad de pienso del cuenco. A firebase le enviamos el
+        menor valor de los 2 ya que suponemos que a en el caso de que uno de los resultados sea erroneo se envia mas bajo
+        para así rellenar la comida cuanto antes
+        
+        Args:
+            ---
+       """
 
         if self.analize == True:
+            print("Analize es true")
 
             try:
                 # Seleccionamos bgr8 porque es la codificacion de OpenCV por defecto
                 cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+                print("Se ejecuta")
+
             except CvBridgeError as e:
                 print(e)
 
@@ -317,13 +144,13 @@ class Service(Node):
             lower_brown = np.array([0,140,0])#rango de color marrón que se capta
             upper_brown = np.array([255,255,255])
 
-            #cv2.imshow('hsv',hsv)
+            cv2.imshow('hsv',hsv)
 
             mask = cv2.inRange(hsv, lower_brown, upper_brown)
-            #cv2.imshow('mask',mask)
+            cv2.imshow('mask',mask)
 
             res = cv2.bitwise_and(img2, img2, mask= mask)
-            #cv2.imshow('res',res)
+            cv2.imshow('res',res)
 
             #cantidad de pixeles marrones
             total = res.size
@@ -376,43 +203,59 @@ class Service(Node):
                 print(" resultado del metodo")
                 print(self.resultado)
 
-            return self.resultado
+                return self.resultado
+
+        # subir imagen a Storage
+            storage = firebase.storage()
+
+            my_image = "/home/ruben/turtlebot3_ws/src/petsitter-main/petsitter_web/imgs/Imagen_cuenco.jpg"
+            image_path = "/images/Imagen_cuenco.jpg" # ruta donde se guarda la imagen
+
+            # Upload image
+            storage.child(image_path).put(my_image)
+            img_url = storage.child(image_path).get_url(None)
+
+            fecha_actual = datetime.now()
+            fecha = fecha_actual.strftime("%d/%m/%Y %H:%M")
+
+            # subir documento a Firestore
+            db = firestore.client()
+
+            doc_ref = db.collection(u'Imagenes').document(u'cuenco')
+            doc_ref.set({
+                u'url' : img_url,
+                u'cantidad' : self.resultado,
+                u'fecha' : fecha
+            })
+
+            self.analize = False
+
+
 
 
     def my_first_service_callback(self, request, response):
-        # recibe los parametros de esta clase
-        #  recibe el mensaje request
-        # devuelve el mensaje response
-
-        # crea un mensaje tipo Twist 
-        #msg = Twist()
-
+        """ 
+        Si se recibe la orden de analizar, se le poner self.analize a true
+        
+        Args:
+            request (string): Contiene la orden de comenzar a analizar
+            response (bool): Indica si se ha obtenido la orden y es válida
+        """
         if request.analize == True:
+            #print("Detecta pienso")
             self.analize = True
-            # publica el mensaje
-            #self.publisher.publish(msg)
-            # imprime mensaje informando del movimiento
-            self.get_logger().info('Analizando foto')
-            # devuelve la respuesta
             response.success = True
-     
         else:
-            # estado de la respuesta
-            # si no se ha dado ningun caso anterior
-            self.analize = False
-            self.get_logger().info('No se ha analizado')
             response.success = False
-
-        # devuelve la respuesta
+            self.analize = False
         return response
-#final de la clase
-
+        
 
 def main(args=None):
     # inicializa la comunicacion ROS2
     rclpy.init(args=args)
     # creamos el nodo
-    service = Service()
+    service = ServiceOpencv()
     try:
         #dejamos abierto el servicio
         rclpy.spin(service)
@@ -426,7 +269,7 @@ def main(args=None):
         rclpy.shutdown()
 
 cv2.destroyAllWindows()
+    
 #definimos el ejecutable
-#if __name__=='__main__':
-#    main()
-
+if __name__=='__main__':
+    main()
